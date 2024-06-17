de7ff5e15677412fd9cb654bbdf3682e6e7a813c
abenezer-s <abenezersisay7@gmail.com>
2024-06-17 14:38:47 +0300

    task 3

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,8 @@
+# Convert Hours to Seconds
+
+hours = 2 
+num_secs = 3600   # number of seconds on an hour
+
+seconds = hours * num_secs   #convert minutes to seconds
+
+print("{} hour(s) is {} seconds".format(hours, seconds))
