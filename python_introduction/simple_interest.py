2d765280638067284c59005d80bc198589183cb6
abenezer-s <abenezersisay7@gmail.com>
2024-06-17 14:25:34 +0300

    task 1

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+#Apply basic Python arithmetic operations and variable assignments 
+#to calculate the simple interest on a given principal amount, rate of interest, and time.
+
+principal = 1000    # amount expressed in dollars    
+rate = 0.05         # 5% interest rate
+time = 3            # time expressed in years        
+
+interest = principal * rate * time
+
+print("The simple interest is: {}".format(interest))
