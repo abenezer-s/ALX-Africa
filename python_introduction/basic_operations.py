1889ae96529803657f34ec2799674c19203194d1
abenezer-s <abenezersisay7@gmail.com>
2024-06-17 14:16:58 +0300

    task 0

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,14 @@
+
+#Print the results of each operation in the format: [operation] of [number1] and [number2] is [result].
+number1 = 10
+number2 = 5
+#sum
+result = number1 + number2
+print("sum of {} and {} is {}".format(number1, number2, result))
+#subtract
+result = number1 - number2
+print("subtraction of {} and {} is {}".format(number1, number2, result))
+#multiply
+result = number1 * number2
+print("multiplication of {} and {} is {}".format(number1, number2, result))
+
