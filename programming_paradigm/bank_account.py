3eaacb43b8b4bf6e612fbb53cb273168b389bc47
abenezer-s <abenezersisay7@gmail.com>
2024-07-09 22:15:36 +0300

    bank_account.py: $

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -21,5 +21,5 @@ class BankAccount:
             return False   
         
     def display_balance(self):
-        return print(f"Current Balance: {self.account_balance}")
+        return print(f"Current Balance: ${float(self.account_balance)}")
     
