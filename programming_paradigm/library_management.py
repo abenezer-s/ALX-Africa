0f61c0f1bfcf181dcf0bec0cf78d91cf7064f229
abenezer-s <abenezersisay7@gmail.com>
2024-07-13 22:05:43 +0300

    library_management.py: pass

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,24 +1,27 @@
 # Implementing Basic OOP for a Library Management System
 class Book:
 
-        def __init__(self, title, author):
-            self.title = title
-            self.author = author
-            self._is_checked_out = False
-        
-        def get_title(self):
-             return self.title
-        
-        def get_author(self):
-            return self.author
-        
-        def set_status(self, status):
-            self._is_checked_out = status
-
-        def get_status(self):
-            return self._is_checked_out 
-        def __str__(self):
+     def __init__(self, title, author):
+         self.title = title
+         self.author = author
+         self._is_checked_out = False
+     
+     def get_title(self):
+          return self.title
+     
+     def get_author(self):
+         return self.author
+     
+     def set_status(self, status):
+         self._is_checked_out = status
+     def get_status(self):
+         return self._is_checked_out 
+     def __str__(self):
              return f"{self.title} by {self.author}"
+     def return_book(self, title):
+          pass
+     def check_out_book(self, title):
+          pass
 
 class Library:
       
