f651a0d155cac905c3663129b618f1aeba05b3f9
abenezer-s <abenezersisay7@gmail.com>
2024-07-23 20:58:24 +0300

    MySQLServer.py

---
+++ MySQLServer.py
@@ -0,0 +1,17 @@
+import mysql.connector
+try:
+    mydb = mysql.connector.connect(
+        host = 'localhost',
+        user = 'alx_db_user',
+        passwd = 'password',
+        collation = 'utf8mb4_general_ci'
+    )
+except:
+    raise Exception("could not connect to DB")
+
+cursor = mydb.cursor()
+try:
+    cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+    print('Database alx_book_store created successfully!')
+except:
+    raise Exception("coudln't create DB") 
