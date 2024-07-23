22b9ecfbc1be9b072052e207940afda09ba98306
abenezer-s <abenezersisay7@gmail.com>
2024-07-23 21:52:37 +0300

    task_3: .sql

--- task_3.sql
+++ task_3.sql
@@ -1,18 +1,5 @@
-import mysql.connector
-try:
-    mydb = mysql.connector.connect(
-        host = 'localhost',
-        user = 'alx_db_user',
-        passwd = 'password',
-        collation = 'utf8mb4_general_ci',
-        database = 'alx_book_store'
-    )
 
-except mysql.connector.Error:
-    raise Exception("could not connect to DB")
 
-cursor = mydb.cursor()
+USE alx_book_store;
 
-cursor.execute("""
-    SHOW TABLES
-""")
+SHOW TABLES
