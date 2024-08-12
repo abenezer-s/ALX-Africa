a2558be7e2c4e9326e1bfcf22396473a3a504ded
abenezer-s <abenezersisay7@gmail.com>
2024-08-12 18:29:51 +0300

    task 0

---
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,9 @@
+from .models import *
+#Query all books by a specific author.
+qs = Book.objects.filter(author__name="george orwell")
+#List all books in a library.
+books = Book.objects.all()
+#Retrieve the librarian for a library.
+library = Library.objects.get(name= "library name")
+librarian_name = library.librarian.name
+
