9f316527b56673116cac4d749044386b5dd10dff
abenezer-s <abenezersisay7@gmail.com>
2024-08-13 15:34:03 +0300

    library import

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import DetailView
 from django.shortcuts import render
-from .models import Book, Library
+from .models import Book
+from .models import Library
 
 # Create your views here.
 
