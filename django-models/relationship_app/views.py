e2dbe7cc2077fc18dafdc42a43511e65882c3189
abenezer-s <abenezersisay7@gmail.com>
2024-08-13 20:14:08 +0300

    response

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import user_passes_test
+from django.http import HttpResponse
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from django.views.generic.detail import DetailView
@@ -13,19 +14,21 @@ def is_admin(user):
     return user.userprofile.role == 'Admin'
 @user_passes_test(is_admin)
 def admin_view(request):
-    pass
+    return HttpResponse('admin')
 
 def is_librarian(user):
     return user.userprofile.role == 'Librarian'
 @user_passes_test(is_librarian)
 def librarian_view(request):
-    pass
+        return HttpResponse('librarian')
+
 
 def is_member(user):
     return user.userprofile.role == 'Member'
 @user_passes_test(is_member)
 def member_view(request):
-    pass
+    return HttpResponse('member')
+
 
 def login_view(request):
     if request.method == "POST":
