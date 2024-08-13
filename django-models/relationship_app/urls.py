9c0c2c1682b7a798997c5d0275e33fcaba8aa111
abenezer-s <abenezersisay7@gmail.com>
2024-08-13 18:19:10 +0300

    authentication

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,6 +1,12 @@
 from .views import list_books, LibraryDetailView
+from .views import SignUpView, LibraryLoginView, LibraryLogoutView
 from django.urls import path
+
+
 urlpatterns = [
-    path('/books', list_books, name='book-view'),
-    path('/library', LibraryDetailView.as_view, name='library-details')
+    path('login/', LibraryLoginView.as_view(), name='login'),
+    path('logout/', LibraryLogoutView.as_view(), name='logout'),
+    path('register/', SignUpView.as_view(), name='register-view'),
+    path('books/', list_books, name='book-view'),
+    path('library/', LibraryDetailView.as_view, name='library-details')
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 from django.views.generic.detail import DetailView
 from django.shortcuts import render
 from .models import Book
@@ -16,3 +20,17 @@ def  list_books(request):
 class LibraryDetailView(DetailView):
     model =Library
     template_name = 'relationship_app/library_detail.html'
+
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'relationship_app/registration.html'
+
+class LibraryLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+    redirect_authenticated_user = True
+
+class LibraryLogoutView(LogoutView):
+    next_page = 'relationship_app/login'  
+
