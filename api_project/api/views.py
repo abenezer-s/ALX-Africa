739c393d5ccc7a0342a37c158ba19ac4de024c08
abenezer-s <abenezersisay7@gmail.com>
2024-08-26 18:44:26 +0300

    added permissions.IsAuthenticated

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework import viewsets 
-from rest_framework.permissions import IsAdminUser
-from rest_framework import permissions
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+# from rest_framework.permissions.IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
 # Create your views here.
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes= [permissions.IsAuthenticated, IsAdminUser] #Only users that are authenticated and are admin users can access this view
+    permission_classes= [IsAuthenticated, IsAdminUser] #Only users that are authenticated and are admin users can access this view
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes= [permissions.IsAuthenticated, IsAdminUser] #Only users that are authenticated and are admin users can access this view
+    permission_classes= [IsAuthenticated, IsAdminUser] #Only users that are authenticated and are admin users can access this view
