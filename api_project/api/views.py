77e4dc1b8a66ee95ed28c5ada94bcadddcc21189
abenezer-s <abenezersisay7@gmail.com>
2024-08-26 18:40:59 +0300

    imported IsAuthernticated separatly

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework import viewsets 
-from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
 # Create your views here.
