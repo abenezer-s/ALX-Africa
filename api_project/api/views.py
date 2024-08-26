7b9b7066ef1ff99dd8da73e714b7854a4e802831
abenezer-s <abenezersisay7@gmail.com>
2024-08-26 18:52:19 +0300

    fixed default permissions and auth settings

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from rest_framework import generics
 from rest_framework import viewsets 
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
-from rest_framework.permissions.IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
 # Create your views here.
--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -124,7 +124,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES" : [
+        'rest_framework.authentication.TokenAuthentication',
 
+    ],
+    "DEFAULT_PERMISSION_CLASSES" : [
+        'rest_framework.permissions.IsAuthenticated',    
+    ]
+}
 DEFAULT_AUTHENTICATION_CLASS = [
-    'rest_framework.authentication.TokenAuthentication',
+    
 ]
