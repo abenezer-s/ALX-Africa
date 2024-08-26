15bf23e2ba63be25f2f3afe929dfbeefef16354d
abenezer-s <abenezersisay7@gmail.com>
2024-08-26 18:38:46 +0300

    added token auth in default default auth class

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -124,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DEFAULT_AUTHENTICATION_CLASS = [
+    'rest_framework.authentication.TokenAuthentication',
+]
