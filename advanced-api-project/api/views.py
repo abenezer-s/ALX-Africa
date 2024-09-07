5b156fe30df1aadd9e47ffbc43973d0e667c6764
abenezer-s <abenezersisay7@gmail.com>
2024-09-07 11:49:08 +0300

    custom generic drf views

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,23 +1,38 @@
 from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
 from .models import Book
 from .serializers import BookSerializer
-
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+#list all books
 class BookListView(ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
+#decribes a book
 class BookDetailView(RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
+#adds a new book    
 class BookCreateView(CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-
+    permission_classes = [IsAuthenticated]
+    def perform_create(self, serializer):
+       # Add custom validation or processing here
+       serializer.save()
+#updates a book
 class BookUpdateView(UpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+    def perform_update(self, serializer):
+        # Add custom validation or processing here
+        serializer.save()
 
+#deletes a book
 class BookDeleteView(DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
