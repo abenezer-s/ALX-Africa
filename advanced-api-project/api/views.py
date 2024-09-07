1bf90a61013baa52ef06469235985dd526677ec1
abenezer-s <abenezersisay7@gmail.com>
2024-09-07 12:08:48 +0300

    flitering and searching

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,11 +2,20 @@ from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView,
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
+
 #list all books
 class BookListView(ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    
+    #filtering and searcing
+    filterset_fields = ['title', 'author', 'publication_year']
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['title', 'author']
+    ordering_fields = ['title', 'publication_year']
 
 #decribes a book
 class BookDetailView(RetrieveAPIView):
