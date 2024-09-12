5f9468c163f9d6ba0b361cb899dec3419155fad3
abenezer-s <abenezersisay7@gmail.com>
2024-09-12 14:51:25 +0300

    tagwidget

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Post, Comment, Tag 
+from taggit.forms import TagWidget as BaseTagWidget
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -22,7 +23,9 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
-
+        widgets = {
+            'tags': TagWidget(),  # Use the custom widget for the tags field
+        }
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
@@ -30,3 +33,8 @@ class CommentForm(forms.ModelForm):
         widgets = {
             'content': forms.Textarea(attrs={'rows': 4, 'cols': 40}),
         }
+
+class TagWidget(BaseTagWidget):
+    def __init__(self, attrs=None):
+        super().__init__(attrs)
+        self.attrs.update({'placeholder': 'Add tags separated by commas'})
