6ddfa8fd18e20f2ebdc1aeb6974873ffd4d89cfd
abenezer-s <abenezersisay7@gmail.com>
2024-09-22 17:25:07 +0300

    db config

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -83,10 +83,15 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'localhost',
+        'PORT': '5433',  
     }
 }
+
 
 
 # Password validation
